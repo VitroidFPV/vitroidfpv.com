99fdbe01146bd38403bd0ea27997e3a85763d96e
VitroidFPV <vitroid05@gmail.com>
2022-04-08 23:30:47 +0200

    Full home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,33 @@
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{html,js,svelte}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'highlight': '#90d95b',
+        'highlight-dark': '#2ad162',
+        'red':    '#d6395b',
+        'orange': '#ff9742',
+        'yellow': '#ffcc00',
+        'green':  '#5ae65a',
+        'cyan': '#5ad8e6',
+        'blue': '#5a6fe6',
+        'violet': '#9550ba',
+        'main': {
+          100: '#3d454d',
+          200: '#292e33',
+          300: '#1e1e1e',
+          400: '#0f0f0f',
+        },
+        'contrast': {
+          100: '#eeeeee',
+          200: '#e2e2e2',
+          300: '#e0e0e0',
+          400: '#cccccc',
+        },
+      },
+    },
+
   },
   plugins: [],
 }
