eb23ada8c91319d55b06bd14c875967a36aeacd9
VitroidFPV <vitroid05@gmail.com>
2022-04-13 17:54:14 +0200

    First style

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -1,9 +1,24 @@
 <script>
-    import Faq from "../components/faqPage/Faq.svelte";
     import Header from "../components/Header.svelte";
     import { onMount } from 'svelte';
     import MainHeader from "../components/mainHeader.svelte";
     import CategoryIndex from "../components/faqPage/categoryIndex.svelte";
+
+    console.log("===========");
+    const modules = import.meta.globEager("/faq/*.md");
+    // console.log(modules)
+    let grouped_modules = {};
+
+    for (const k in modules) {
+        const cat = modules[k].metadata.Category;
+        if (grouped_modules[cat]) {
+        grouped_modules[cat].push(modules[k]);
+        } else {
+        grouped_modules[cat] = [modules[k]]
+        }
+    }
+        
+    console.log(grouped_modules)
 
     onMount(() =>{
         var coll = document.getElementsByClassName("collapsible");
@@ -23,12 +38,18 @@
     })
 </script>
 
+<svelte:head>
+    <title>VitroidFPV - FAQ</title>
+</svelte:head>
+
 <div class="w-full min-h-screen m-8 flex flex-col">
+
     <MainHeader text="FAQ"/>
     <Header text="If you need a quick answer, there's a good chance you'll find it here!"></Header>
     <p class="leading-normal">There's a lot of questions in FPV, doesn't matter if you're just starting or not<br>
     You will find most of the common and easy to answer ones here. For more specific ones, there are tutorials planned<br>
     The questions are sorted into individual categories, and next to each one there is a button that will copy a link to the specific one if you want to send it to someone</p>
+
     <ul class="mt-8 text-2xl w-fit">
         <CategoryIndex icon="bg-[url('https://img.icons8.com/material-outlined/32/9550ba/plus--v1.png')]" color="hover:text-violet" text="Starting out"></CategoryIndex>
         <CategoryIndex icon="bg-[url('https://img.icons8.com/material-outlined/32/d6395b/plus--v1.png')]" color="hover:text-red" text="Building"></CategoryIndex>
@@ -37,6 +58,28 @@
         <CategoryIndex icon="bg-[url('https://img.icons8.com/material-outlined/32/5ad8e6/plus--v1.png')]" color="hover:text-cyan" text="Radio"></CategoryIndex>
         <CategoryIndex icon="bg-[url('https://img.icons8.com/material-outlined/32/5a6fe6/plus--v1.png')]" color="hover:text-blue" text="Hardware"></CategoryIndex>
     </ul>
-    <Faq category="violet" title="aaa" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-    <Faq category="red" title="aaa" content="bbb"/>
+
+    {#each Object.entries(grouped_modules) as [cat, contents]}
+    <div class="{cat} my-8 w-full h-fit">
+        {#each contents as question}
+        <div class="w-full">
+            <div class="text-3xl tracking-tight border-b-2 w-fit px-1 cat {cat} pb-1">{cat}</div>
+            <div class="w-[32rem] tracking-normal text-main-200 dark:text-contrast-100 border-none">
+                <button type="button" class="collapsible duration-300 after:ml-1 text-[22px] hover:translate-x-1 duartion-150 {cat}">{question.metadata.title}</button>
+                <div class="content text-xl">
+                    <p>{question.metadata.answer}</p>
+                </div>
+            </div>
+        </div>
+        {/each}
+    </div>
+    {/each}
 </div>
+
+<style>
+    .content {
+      max-height: 0;
+      overflow: hidden;
+      transition: max-height 0.2s ease-out;
+    }
+</style>
