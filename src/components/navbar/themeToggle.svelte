96b0bd8c93ea716c12bbf52c3d07522ae97e3dd9
VitroidFPV <vitroid05@gmail.com>
2022-04-18 21:10:10 +0200

    Theme switch working

--- src/components/navbar/themeToggle.svelte
+++ src/components/navbar/themeToggle.svelte
@@ -1,8 +1,21 @@
 <script>
-    function toggle() {
-        window.document.body.classList.toggle("dark")
+  import { writable } from 'svelte/store';
+  import { persist, localStorage } from '@macfja/svelte-persistent-store';
+  import { onMount } from 'svelte';
+
+  const theme = persist(writable('light'), localStorage(), 'app-theme');
+
+  onMount(() => {
+    let unsubscribe = theme.subscribe((value) => {
+      if (value === "dark") { window.document.body.classList.add("dark"); }
+      else { window.document.body.classList.remove("dark"); }
+    });
+
+    return () => {
+      unsubscribe()
     }
+  });
 </script>
 
-<button on:click={toggle} class="h-16 w-16 bg-[url('https://img.icons8.com/material-outlined/64/a1a1a1/sun--v1.png')] scale-75 dark:bg-[url('https://img.icons8.com/material-outlined/64/a1a1a1/moon--v1.png')]">
+<button on:click={() => $theme = ($theme == 'dark' ? 'light' : 'dark')} class="absolute right-[5%] bottom-[10px] bg-contain bg-no-repeat md:static justify-self-end mb-4 h-16 w-16 bg-[url('https://img.icons8.com/material-outlined/64/a1a1a1/sun--v1.png')] md:scale-75 scale-50 dark:bg-[url('https://img.icons8.com/material-outlined/64/a1a1a1/moon--v1.png')]">
 </button>
