68b8be5d02208c1d63967e7061c3621d974f08a1
VitroidFPV <vitroid05@gmail.com>
2022-04-20 16:09:49 +0200

    added Footer

---
+++ src/components/navbar/Footer.svelte
@@ -0,0 +1,99 @@
+<div class="flex flex-col">
+    <!-- <div class="flex flex-col justify-start h-32">
+        <div class="max-h-fit max-w-full scale-y-50">
+            <svg class="max-w-full h-fit" id="visual" viewBox="0 0 2400 300" width="2400" height="300" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+            <rect x="0" y="0" width="2400" height="300" class="fill-transparent"></rect>
+            <path d="M0 92L50 91.8C100 91.7 200 91.3 300 112.7C400 134 500 177 600 175.5C700 174 800 128 900 118.5C1000 109 1100 136 1200 151.8C1300 167.7
+            1400 172.3 1500 158C1600 143.7 1700 110.3 1800 97.7C1900 85 2000 93 2100 114.8C2200 136.7 2300 172.3 2350 190.2L2400 208L2400 301L2350 301C2300
+            301 2200 301 2100 301C2000 301 1900 301 1800 301C1700 301 1600 301 1500 301C1400 301 1300 301 1200 301C1100 301 1000 301 900 301C800 301 700 301
+            600 301C500 301 400 301 300 301C200 301 100 301 50 301L0 301Z" fill="" stroke-linecap="round" stroke-linejoin="miter" class="fill-highlight dark:fill-highlight-dark"></path>
+            </svg>
+        </div>
+    </div> -->
+
+    <div class="max-h-fit max-w-full">
+        <svg class="max-w-full h-fit" id="visual" viewBox="0 0 2400 300" width="2400" height="300" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+            <path d="M0 63L57.2 72.2C114.3 81.3 228.7 99.7 343 104.7C457.3 109.7 571.7 101.3 686 106C800.3 110.7 914.7 128.3 1028.8 132.3C1143 136.3 1257 126.7 1371.2 112.2C1485.3 
+            97.7 1599.7 78.3 1714 77.3C1828.3 76.3 1942.7 93.7 2057 94.7C2171.3 95.7 2285.7 80.3 2342.8 72.7L2400 65L2400 301L2342.8 301C2285.7 301 2171.3 301 2057 301C1942.7 301 
+            1828.3 301 1714 301C1599.7 301 1485.3 301 1371.2 301C1257 301 1143 301 1028.8 301C914.7 301 800.3 301 686 301C571.7 301 457.3 301 343 301C228.7 301 114.3 301 57.2 301L0 
+            301Z" class="fill-highlight dark:fill-highlight-dark"></path>
+            <path d="M0 99L57.2 107.5C114.3 116 228.7 133 343 145.7C457.3 158.3 571.7 166.7 686 167.2C800.3 167.7 914.7 160.3 1028.8 158.8C1143 157.3 1257 161.7 1371.2 161.2C1485.3 
+            160.7 1599.7 155.3 1714 148.7C1828.3 142 1942.7 134 2057 126.2C2171.3 118.3 2285.7 110.7 2342.8 106.8L2400 103L2400 301L2342.8 301C2285.7 301 2171.3 301 2057 301C1942.7 
+            301 1828.3 301 1714 301C1599.7 301 1485.3 301 1371.2 301C1257 301 1143 301 1028.8 301C914.7 301 800.3 301 686 301C571.7 301 457.3 301 343 301C228.7 301 114.3 301 57.2 
+            301L0 301Z" class="fill-[#6bc14c] dark:fill-[#2D9B4D]"></path>
+            <path d="M0 202L57.2 198.7C114.3 195.3 228.7 188.7 343 181.5C457.3 174.3 571.7 166.7 686 164.2C800.3 161.7 914.7 164.3 1028.8 167.5C1143 170.7 1257 174.3 1371.2 
+            176.7C1485.3 179 1599.7 180 1714 181.7C1828.3 183.3 1942.7 185.7 2057 183C2171.3 180.3 2285.7 172.7 2342.8 168.8L2400 165L2400 301L2342.8 301C2285.7 301 2171.3 301 2057 
+            301C1942.7 301 1828.3 301 1714 301C1599.7 301 1485.3 301 1371.2 301C1257 301 1143 301 1028.8 301C914.7 301 800.3 301 686 301C571.7 301 457.3 301 343 301C228.7 301 114.3 
+            301 57.2 301L0 301Z" class="fill-[#439640] dark:fill-[#296938]"></path>
+            <path d="M0 204L57.2 205.7C114.3 207.3 228.7 210.7 343 216.2C457.3 221.7 571.7 229.3 686 232.7C800.3 236 914.7 235 1028.8 232.2C1143 229.3 1257 224.7 1371.2 222.8C1485.3 
+            221 1599.7 222 1714 220.8C1828.3 219.7 1942.7 216.3 2057 219.2C2171.3 222 2285.7 231 2342.8 235.5L2400 240L2400 301L2342.8 301C2285.7 301 2171.3 301 2057 301C1942.7 301 
+            1828.3 301 1714 301C1599.7 301 1485.3 301 1371.2 301C1257 301 1143 301 1028.8 301C914.7 301 800.3 301 686 301C571.7 301 457.3 301 343 301C228.7 301 114.3 301 57.2 301L0 
+            301Z"  class="fill-[#356846] dark:fill-[#274531]"></path>
+            <path d="M0 267L57.2 261.2C114.3 255.3 228.7 243.7 343 242C457.3 240.3 571.7 248.7 686 253.7C800.3 258.7 914.7 260.3 1028.8 262.8C1143 265.3 1257 268.7 1371.2 
+            264.8C1485.3 261 1599.7 250 1714 245.8C1828.3 241.7 1942.7 244.3 2057 246.8C2171.3 249.3 2285.7 251.7 2342.8 252.8L2400 254L2400 301L2342.8 301C2285.7 301 2171.3 301 
+            2057 301C1942.7 301 1828.3 301 1714 301C1599.7 301 1485.3 301 1371.2 301C1257 301 1143 301 1028.8 301C914.7 301 800.3 301 686 301C571.7 301 457.3 301 343 301C228.7 301 
+            114.3 301 57.2 301L0 301Z" class="dark:fill-[#171b18] fill-[#2C4938]"></path>
+          </svg>
+    </div>
+    <div class="w-full min-h-32 dark:bg-[#171b18] bg-[#2C4938] md:pl-48 md:pb-8 pb-28 px-8">
+        <div class="text-contrast-100 flex flex-row justify-around">
+            <div class="text-5xl caveat text-contrast-200 w-64 md:m-0 mr-4">Vitroid FPV</div>
+
+            <div class="md:flex hidden flex-col w-64">
+                <div class="text-2xl">Navigation:</div>
+                <ul class="text-lg">
+                    <li><a href="/" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Home</a></li>
+                    <li><a href="/faq" class="underline font-thin opacity-90 hover:opacity-100 duration-100">FAQ</a></li>
+                    <li><a href="/builds" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Builds</a></li>
+                </ul>
+            </div>
+
+            <div class="flex flex-col w-64">
+                <div class="text-2xl">Socials:</div>
+                <div class="flex flex-row">
+                    <ul class="flex justify-around flex-col">
+                        <li class="w-fit flex">
+                            <a href="/" class="w-fit h-fit flex flex-col opacity-90 hover:opacity-100 duration-100">
+                                <div class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/discord-logo.png')] bg-no-repeat bg-contain bg-center"> </div>
+                                <div class="text-xs opacity-50">Mr Steele</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/" class="w-fit h-fit flex flex-col opacity-90 hover:opacity-100 duration-100">
+                                <div class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/discord-logo.png')] bg-no-repeat bg-contain bg-center"> </div>
+                                <div class="text-xs opacity-50">UAV Futures</div>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="flex justify-around flex-col">
+                        <li class="w-fit flex">
+                            <a href="/" class="w-fit h-fit flex flex-col items-center opacity-90 hover:opacity-100 duration-100 mr-4">
+                                <div class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/youtube-play.png')] bg-no-repeat bg-contain bg-center"></div>
+                            </a>
+                        </li>
+                        <li class="w-fit flex">
+                            <a href="/" class="w-fit h-fit flex flex-col items-center opacity-90 hover:opacity-100 duration-100 mr-4 mb-3">
+                                <div class="h-12 w-12 bg-[url('https://img.icons8.com/ios-filled/64/eeeeee/reddit--v1.png')] bg-no-repeat bg-contain bg-center"></div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="flex flex-col">
+                <div class="text-2xl">Links:</div>
+                <ul class="text-base">
+                    <li><a href="/" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Site GitHub</a></li>
+                    <li><a href="/faq" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Donate</a></li>
+                    <li><a href="/builds" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Icons</a></li>
+                </ul>
+                <div class="text-2xl">Other resources:</div>
+                <ul class="text-base">
+                    <li><a href="/" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Joshua Bardwell</a></li>
+                    <li><a href="/faq" class="underline font-thin opacity-90 hover:opacity-100 duration-100">Oscal Liang</a></li>
+                    <li><a href="/builds" class="underline font-thin opacity-90 hover:opacity-100 duration-100">FPV Market EU</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,13 @@
 <script>
   import "../app.css";
   import Nav from "../components/navbar/Nav.svelte";
+  import Footer from "../components/navbar/Footer.svelte";
 </script>
 
-<div class="max-w-full min-h-screen flex md:flex-row flex-col-reverse dark:bg-main-400 bg-contrast-100 text-main-200 dark:text-contrast-100 text-lg scroll-smooth">
-  <Nav />
-  <slot />
+<div class="flex flex-col dark:bg-main-400 bg-contrast-100 text-main-200 dark:text-contrast-100 text-lg scroll-smooth">
+  <div class="flex md:flex-row flex-col-reverse max-w-full h-fit">
+    <Nav />
+    <slot />
+  </div>
+  <Footer />
 </div>
