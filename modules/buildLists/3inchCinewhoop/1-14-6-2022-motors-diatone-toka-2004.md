f747e7ba474dd9006383d35b266e1aa7e742b9e1
VitroidFPV <vitroid05@gmail.com>
2022-06-14 20:44:55 +0200

    hopefully added full setup for cinewhoop build

---
+++ modules/buildLists/3inchCinewhoop/1-14-6-2022-motors-diatone-toka-2004.md
@@ -0,0 +1,14 @@
+---
+Category: Motors
+color: green
+title: Diatone Toka 2004
+price: $16.99
+text: The lower kv on these motors would work well for bigger, 3.5" props. Or if you want a slower quad overall
+link: https://www.diatone.us/collections/motor/products/mamba-toka-2004-1700kv-2900kv-racing-motor-green
+point1: >-
+ Size: 2004
+point2: >-
+ Weight: 33.5g
+point3: >-
+ KV: 1700/2900
+---
---
+++ src/routes/builds/3inch-cinewhoop.svelte
@@ -0,0 +1,79 @@
+<script>
+        // @ts-ignore
+    import BuildProduct from "/src/components/buildsPage/buildProduct.svelte";
+        // @ts-ignore
+    import MainHeader from "/src/components/mainHeader.svelte";
+        // @ts-ignore
+    import Header from "/src/components/Header.svelte";
+        // @ts-ignore
+    import Paragraph from "/src/components/Paragraph.svelte";
+
+    const modules = import.meta.globEager("/modules/buildLists/3inchCinewhoop/*.md");
+    // console.log(modules)
+    let grouped_modules = {};
+
+    for (const k in modules) {
+        const cat = modules[k].metadata.Category;
+        if (grouped_modules[cat]) {
+            grouped_modules[cat].push(modules[k]);
+        } else {
+            grouped_modules[cat] = [modules[k]]
+        }
+    }
+        
+</script>
+
+<svelte:head>
+    <title>VitroidFPV - 3" Cinewhoop Build</title>
+    <meta name="author" content="VitroidFPV"/>
+    <meta property="og:image" content="https://www.vitroidfpv.com/sources/cinewhoop_512.png">
+    <meta property="og:image:width" content="512">
+    <meta property="og:image:height" content="512">
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="VitroidFPV" />
+    <meta property="article:author" content="VitroidFPV" />
+    <meta property="og:title" content="Builds - 3'' Cinewhoop Build List"/>
+    <meta name="description" content="Meant to be used with a full size gopro, to carry it safely around hard, and soft targets alike, in close proximity. Wider, shorter motors, fully protected frames, and mini stacks or AIOs"/>
+    <meta property="og:description" content="Meant to be used with a full size gopro, to carry it safely around hard, and soft targets alike, in close proximity. Wider, shorter motors, fully protected frames, and mini stacks or AIOs">
+    <meta content="https://vitroidfpv.com/faq" property="og:url" />
+    <meta name="theme-color" content="#1cd167">
+</svelte:head>
+
+<div class="w-full p-4 px-8 flex flex-col">
+    <MainHeader text="3'' Cinewhoop"/>
+    <Header text="Slow, safe, cinematic flying, indoors and outdoors"/>
+    <Paragraph text="Meant to be used with a full size gopro, to carry it safely around hard, and soft targets alike, in close proximity. Wider, shorter motors, fully protected frames, and mini stacks or AIOs<br>
+    This list includes everything to build a full quad, except parts such as cameras, goggles, receiver and radio systems as those vary depending on prefference and you'll find recommendations on this site elsewhere.<br>
+    The price range for each part is highlighted, so you can quickly find something to suit your budget needs">
+    <ul class="flex flex-row md:flex-col justify-around w-full flex-wrap">
+        <li class="before:content-[''] before:pr-7 before:rounded-lg before:mr-2 before:bg-green before:relative mb-2">Budget</li>
+        <li class="before:content-[''] before:pr-7 before:rounded-lg before:mr-2 before:bg-orange before:relative mb-2">Mid-range</li>
+        <li class="before:content-[''] before:pr-7 before:rounded-lg before:mr-2 before:bg-red before:relative mb-2">High-end</li>
+        <li class="before:content-[''] before:pr-7 before:rounded-lg before:mr-2 before:bg-violet before:relative mb-2">Highly varied</li>
+    </ul>
+    </Paragraph>
+
+
+    <div class="flex flex-col">
+        {#each Object.entries(grouped_modules) as [cat, contents]}
+        <div class="{cat} my-8 w-full h-fit">
+        <div class="text-4xl tracking-tight md:w-fit f-full px-1 md:ml-1 ml-2 cat {cat} mb-2 text-center" id="{cat}">{cat}</div>
+            <div class="flex flex-col md:flex-row flex-wrap w-full md:justify-between md:items-start items-center border-b-[1px] border-white/10">
+                {#each contents as info}
+                    <BuildProduct
+                    color="{info.metadata.color}"
+                    title="{info.metadata.title}"
+                    price="{info.metadata.price}"
+                    point1="{info.metadata.point1}"
+                    point2="{info.metadata.point2}"
+                    point3="{info.metadata.point3}"
+                    point4="{info.metadata.point4}"
+                    point5="{info.metadata.point5}"
+                    text="{info.metadata.text}"
+                    link="{info.metadata.link}"/>
+                {/each}
+            </div>
+        </div>
+        {/each}
+    </div>
+</div>
