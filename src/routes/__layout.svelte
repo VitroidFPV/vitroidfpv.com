e9edac478d790bc8967f72d3f484b419d04f4397
VitroidFPV <vitroid05@gmail.com>
2022-07-01 13:58:03 +0200

    this is such a stupid solution, but it might work

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <!-- {#if ready} -->
-  <div class="dark:bg-main-400 bg-contrast-100 text-main-200 dark:text-contrast-100 text-lg scroll-smooth duration-300">
+  <div class:hidden={!ready} class="dark:bg-main-400 bg-contrast-100 text-main-200 dark:text-contrast-100 text-lg scroll-smooth">
     <div class="flex md:flex-row flex-col-reverse h-fit overflow-visible">
       <Nav segment={$page.url.pathname}/>
       <Transition refresh={$page.url.pathname}>
