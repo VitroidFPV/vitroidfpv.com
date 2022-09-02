e5b4a37e75f81e705606a0622e394f5b11df426b
VitroidFPV <vitroid05@gmail.com>
2022-09-02 20:10:17 +0200

    trying stuff

--- src/components/faqPage/faqQuestion.svelte
+++ src/components/faqPage/faqQuestion.svelte
@@ -1,48 +1,63 @@
 <script>
-    let open = false
-    export let title;
-    export let content;
-    export let category;
-    export let id = "";
-    import { onMount } from 'svelte';
-    import { slide } from 'svelte/transition';
-    import { page } from "$app/stores"
+		let open = false
+		export let title;
+		export let content;
+		export let category;
+		export let id = "";
+		import { onMount } from 'svelte';
+		import { slide } from 'svelte/transition';
+		import { page } from "$app/stores"
+		// import SvelteMarkdown from 'svelte-markdown'
 
-    onMount(() => {
-        linkOpen()
-      }
-    )
-    
-    function linkOpen() {
-      let hash = $page.url.hash.replace("#", "");
-      if (hash != "") {
-        if (hash == id) {
-          open = true
-        }
-        location.href = "#" + hash;
-        // document.querySelector("#" + hash).scrollIntoView({behavior: 'smooth'});
-      }
-    }
+		let source = content
 
-    function copyID() {
-      let id = this.nextElementSibling.id;
-      console.log(id)
-      navigator.clipboard.writeText($page.url.origin + $page.url.pathname + "#" + id);
-    }
-    console.log()
+		onMount(() => {
+				linkOpen()
+			}
+		)
+		
+		function linkOpen() {
+			let hash = $page.url.hash.replace("#", "");
+			if (hash != "") {
+				if (hash == id) {
+					open = true
+				}
+				location.href = "#" + hash;
+				// document.querySelector("#" + hash).scrollIntoView({behavior: 'smooth'});
+			}
+		}
+
+		function copyID() {
+			let id = this.nextElementSibling.id;
+			console.log(id)
+			navigator.clipboard.writeText($page.url.origin + $page.url.pathname + "#" + id);
+		}
+		console.log()
+
+	// 	function handleParsed(event) {
+	// 	//access tokens via event.detail.tokens
+	// 	console.log(event.detail.tokens);
+	// }
+
 </script>
 
 <div class="tracking-normal text-main-200 dark:text-contrast-100 border-none flex flex-col mt-2">
-    <div class="flex align-start">
-      <div on:click={copyID} class="text-3xl text-black dark:text-white opacity-10 hover:opacity-30 duration-300 
-      cursor-pointer copy-id mr-2">#</div>
-      <button type="button" on:click={() => open = !open} class:faq-active={open} class="collapsible duration-300 
-        after:ml-1 text-[20px] md:text-xl hover:translate-x-1 duartion-150 {category} text-left" id="{id}">{title}</button>
-    </div>
-    {#if open}
+		<div class="flex align-start">
+			<div on:click={copyID} class="text-3xl text-black dark:text-white opacity-20 hover:opacity-40 duration-300 
+			cursor-pointer copy-id mr-2">#</div>
+			<button type="button" on:click={() => open = !open} class:faq-active={open} class="collapsible duration-300 
+				after:ml-1 text-[20px] md:text-xl hover:translate-x-1 duartion-150 {category} text-left" id="{id}">{title}</button>
+		</div>
+		{#if open}
 			<div transition:slide class="content text-xl bg-contrast-100 dark:bg-main-300 rounded-2xl
 				mb-2 h-full min-w-fit max-w-[70vw]">
-          <p class="md:p-8 p-6 text-base md:text-lg leading-relaxed md">{@html content}</p>
-      </div>
-    {/if}
+					<!-- <div class="md:p-8 p-6 text-base md:text-lg leading-relaxed md"><SvelteMarkdown {source} on:parsed={handleParsed}/></div> -->
+					<div class="md:p-8 p-6 text-base md:text-lg leading-relaxed md">{@html content}</div>
+			</div>
+		{/if}
 </div>
+
+<style>
+	.img-closed { width: 75% }
+	.img-open { width: 100% }
+</style>
