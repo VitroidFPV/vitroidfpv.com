77ccf6ef80677efdfd1f6d1114de4944057baf66
VitroidFPV <vitroid05@gmail.com>
2022-10-05 22:46:13 +0200

    hmmmm

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -66,6 +66,10 @@
 		"There's a lot of questions in FPV, doesn't matter if you're just starting or not. You will find most of the common and easy to answer ones here. For more specific ones, there are tutorials planned";
 </script>
 
+<script context="module">
+	export const prerender = true;
+</script>
+
 <svelte:head>
 	<title>VitroidFPV{title}</title>
 	<meta name="author" content="VitroidFPV" />
@@ -156,8 +160,8 @@
 		</div>
 	{/each}
 
-	<form class="flex flex-col form mt-10" method="post" name="faq" data-netlify="true">
-
+	<form class="flex flex-col form mt-10" method="post" name="faq" netlify netlify-honeypot="bot-field">
+		<input type="hidden" name="form-name" value="faq" />
 		<Header text="Feedback" />
 		<Paragraph text="If you found this page useful (or not), or have any suggestions, ideas and so on, let me know here to help me improve the site!" />
 		<div class="mt-4 flex flex-col">
--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,13 @@ import mdsvexConfig from "./mdsvex.config.js";
 const config = {
 	kit: {
 		adapter: adapter(),
+		target: "#svelte",
+		prerender: {
+			crawl: true,
+			enabled: true,
+			force: true,
+			pages: ['*'],
+		},
 		vite: {
 			server: {
 				fs: {
