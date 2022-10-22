403a562eaf51f24239df95e06e1b5ef7c06f2e82
VitroidFPV <vitroid05@gmail.com>
2022-10-22 17:35:36 +0200

    nothing important

---
+++ src/components/Analytics.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/stores'
+
+	$: {
+	if (typeof gtag !== 'undefined') {
+		gtag('config', 'G-YYT7E26JTN', {
+		page_title: document.title,
+		page_path: $page.url.pathname,
+		})
+	}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-YYT7E26JTN"></script>
+	<script>
+		window.dataLayer = window.dataLayer || []
+
+		function gtag() {
+			dataLayer.push(arguments)
+		}
+
+		gtag('js', new Date())
+		gtag('config', 'G-YYT7E26JTN')
+	</script>
+</svelte:head>
