82776d622a6d3561dd5a56520ab8e70bd8700ace
VitroidFPV <vitroid05@gmail.com>
2022-11-08 07:41:12 +0000

    whole article card is now link

--- src/components/articlesPage/ArticlePreview.svelte
+++ src/components/articlesPage/ArticlePreview.svelte
@@ -32,18 +32,20 @@
 </script>
 
 <div class="article-card flex flex-col h-56 w-96 {category} rounded-[18px] mr-12 my-6 duration-300">
+	<a href={link} class="flex flex-col w-full h-full">
 	<div class="relative flex items-end h-full w-full group overflow-hidden rounded-2xl">
-		<div class="flex flex-col z-20 pb-4 pl-4 text-contrast-50">
-			<div>
-				<div>{category}</div>
-				<Link {link} size="2" color={categoryColor} color1="">{title}</Link>
+			<div class="flex flex-col z-20 pb-4 pl-4 text-contrast-50">
+				<div>
+					<div>{category}</div>
+					<Link {link} size="2" color={categoryColor} color1="">{title}</Link>
+				</div>
+				<div class="">{description}</div>
+				<div class="opacity-0 h-0 mt-0 group-hover:opacity-100 group-hover:h-6 group-hover:mt-2 duration-300 text-base">{date}</div>
 			</div>
-			<div class="">{description}</div>
-			<div class="opacity-0 h-0 mt-0 group-hover:opacity-100 group-hover:h-6 group-hover:mt-2 duration-300 text-base">{date}</div>
+			<div class="card-gradient {category} w-full h-full absolute z-10 backdrop-blur-[3px] group-hover:backdrop-blur-[1px] duration-300 rounded-2xl"></div>
+			<div style="background-image: url({img});"
+				class="article-card-img bg-center bg-cover group-hover:scale-105 duration-300 h-full w-full saturate-0 absolute brightness-75 rounded-2xl">
+			</div>
 		</div>
-		<div class="card-gradient {category} w-full h-full absolute z-10 backdrop-blur-[3px] group-hover:backdrop-blur-[1px] duration-300 rounded-2xl"></div>
-		<div style="background-image: url({img});"
-			class="article-card-img bg-center bg-cover group-hover:scale-105 duration-300 h-full w-full saturate-0 absolute brightness-75 rounded-2xl">
-		</div>
-	</div>
+	</a>
 </div>
