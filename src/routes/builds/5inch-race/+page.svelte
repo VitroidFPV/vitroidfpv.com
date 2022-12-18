e35cc6d2481ed9487e5f74e7dfbdf8f0926b1aee
VitroidFPV <vitroid05@gmail.com>
2022-12-18 17:48:14 +0100

    Hopefully implemented build recommendations

--- src/routes/builds/5inch-race/+page.svelte
+++ src/routes/builds/5inch-race/+page.svelte
@@ -15,23 +15,63 @@
 	let grouped_modules = {};
 
 	for (const k in modules) {
+		// construct object as {category: {group: [metadata]}}
 		const cat = modules[k].metadata.Category;
+		const group = modules[k].metadata.group;
+
 		if (grouped_modules[cat]) {
-			grouped_modules[cat].push(modules[k]);
+			if (grouped_modules[cat][group]) {
+				grouped_modules[cat][group].push(modules[k]);
+			} else {
+				grouped_modules[cat][group] = [modules[k]];
+			}
 		} else {
-			grouped_modules[cat] = [modules[k]];
+			grouped_modules[cat] = {};
+			grouped_modules[cat][group] = [modules[k]];
 		}
 	}
+	// console.log(JSON.stringify(grouped_modules, null, 1));
 
-	//  if key visible isn't in the metadata, set it to true, otherwise set it to the value in the metadata
-	for (const [key, value] of Object.entries(grouped_modules)) {
-		for (const [key2, value2] of Object.entries(value)) {
-			if (value2.metadata.visible == undefined) {
-				value2.metadata.visible = true;
+	// in grouped_modules, from the category "Recommended Builds", log all of the groups, and the value of the key "recommended"
+	let recommended_titles = {};
+	for (const cat in grouped_modules) {
+		if (cat === "Recommended Builds") {
+			for (const group in grouped_modules[cat]) {
+				recommended_titles[group] = grouped_modules[cat][group][0].metadata.recommended;
 			}
 		}
 	}
+	// console.log(JSON.stringify(recommended_titles, null, 1));
 
+	// for each group in recommended_titles, in the arrays there are titles
+	//  find the product in the grouped_modules object that matches the title and log it in its group
+	let recommended_products = {};
+	for (const group in recommended_titles) {
+		recommended_products[group] = [];
+		for (const title of recommended_titles[group]) {
+			for (const cat in grouped_modules) {
+				for (const group2 in grouped_modules[cat]) {
+					for (const product in grouped_modules[cat][group2]) {
+						if (grouped_modules[cat][group2][product].metadata.title === title) {
+							recommended_products[group].push(grouped_modules[cat][group2][product]);
+						}
+					}
+				}
+			}
+		}
+	}
+	console.log(JSON.stringify(recommended_products, null, 1));
+
+	//  if key visible isn't in the metadata, set it to true, otherwise set it to the value in the metadata
+	for (const cat in grouped_modules) {
+		for (const group in grouped_modules[cat]) {
+			for (const product in grouped_modules[cat][group]) {
+				if (!grouped_modules[cat][group][product].metadata.visible) {
+					grouped_modules[cat][group][product].metadata.visible = true;
+				}
+			}
+		}
+	}
 	let prefix = "Builds";
 	let titleRaw = "5\" Race Quad";
 	let title = " - " + titleRaw;
@@ -86,14 +126,51 @@
 
 	<div class="flex flex-col">
 		{#each Object.entries(grouped_modules) as [cat, contents]}
-			<div class="{cat} my-8 w-full h-fit">
-				<div
-					class="text-4xl tracking-tight md:w-fit f-full px-1 md:ml-1 ml-2 cat {cat} mb-2 text-center"
-					id={cat}>
-					{cat}
+			{#if cat !== "Recommended Builds"}
+				<div class="{cat} my-8 w-full h-fit">
+					<div
+						class="text-4xl tracking-tight md:w-fit f-full px-1 md:ml-1 ml-2 cat {cat} mb-2 text-center"
+						id={cat}>
+						{cat}
+					</div>
+					{#each Object.entries(contents) as [group, products]}
+						<div
+							class="ml-3 flex flex-col md:flex-row flex-wrap w-full md:justify-start md:items-start items-center border-b-[1px] border-white/10">
+							{#each products as info}
+								{#if info.metadata.visible}
+									<BuildProduct
+										color={info.metadata.color}
+										title={info.metadata.title}
+										price={info.metadata.price}
+										point1={info.metadata.point1}
+										point2={info.metadata.point2}
+										point3={info.metadata.point3}
+										point4={info.metadata.point4}
+										point5={info.metadata.point5}
+										text={info.metadata.text}
+										link={info.metadata.link}
+										category={cat}
+									/>
+								{/if}
+							{/each}
+						</div>
+					{/each}
 				</div>
-				<div
-					class="ml-3 flex flex-col md:flex-row flex-wrap w-full md:justify-start md:items-start items-center border-b-[1px] border-white/10">
+			{/if}
+		{/each}
+
+		{#each Object.entries(recommended_products) as [group, contents]}
+			<div class="group {group} my-4 w-full h-fit">
+
+				{#if group != "undefined"}
+					<div
+						class="text-xl tracking-tight w-full px-1 {group} mb-2 border-b-[1px] border-gray-700 text-main-50 dark:text-contrast-500"
+						id={group}>
+						{group}
+					</div>
+				{/if}
+
+				<div class="flex flex-col md:flex-row flex-wrap w-full md:justify-start md:items-start items-center">
 					{#each contents as info}
 						{#if info.metadata.visible}
 							<BuildProduct
@@ -107,7 +184,7 @@
 								point5={info.metadata.point5}
 								text={info.metadata.text}
 								link={info.metadata.link}
-								category={cat}
+								category={group}
 							/>
 						{/if}
 					{/each}
