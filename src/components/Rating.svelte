99482dbad6fc73e4d2aa5beebfc8cff454ae4dca
VitroidFPV <vitroid05@gmail.com>
2022-12-20 23:21:23 +0100

    Rating description now shouldn't go under nav

--- src/components/Rating.svelte
+++ src/components/Rating.svelte
@@ -6,7 +6,7 @@
     +<slot/>
     <span class="w-6 h-6 rounded-full bg-green/80 hover:bg-green select-none duration-300 
         text-black pl-2 text-base relative group">?
-        <div class="absolute -right-8 dark:bg-black/50 bg-black/20 backdrop-blur-md scale-0 
+        <div class="absolute -right-40 top-8 dark:bg-black/50 bg-black/20 backdrop-blur-md scale-0 
 		group-hover:scale-100 duration-300 md:rounded-r-2xl md:rounded-bl-2xl rounded-2xl w-96 text-white 
 		text-lg p-4 select-auto shadow-2xl border-2 border-white/10 shadow-black/50"><slot /> people have found this page useful! <br> Give a rating at the bottom of the page</div>
     </span>
