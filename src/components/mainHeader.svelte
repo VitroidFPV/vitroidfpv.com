99f9ac084c58ffa4dcd5ddc812eae830853a53bc
VitroidFPV <vitroid05@gmail.com>
2022-12-22 22:13:48 +0100

    Stuff

--- src/components/mainHeader.svelte
+++ src/components/mainHeader.svelte
@@ -1,3 +1,3 @@
-<h1 class="flex items-center text-6xl md:mx-0 mx-auto mb-4 w-fit px-1 border-b-4 border-highlight dark:border-highlight-dark">
+<h1 class="flex flex-col md:flex-row items-center text-6xl md:mx-0 mx-auto mb-4 w-fit px-1 border-b-4 border-highlight dark:border-highlight-dark text-center">
 	<slot />
 </h1>
