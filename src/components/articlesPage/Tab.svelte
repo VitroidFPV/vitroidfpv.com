d389a3c3043ceef03d854097a5e4b90f7d9bf5ec
VitroidFPV <vitroid05@gmail.com>
2023-03-05 16:38:03 +0100

    Feature: Link directly to opened tabs in the page

--- src/components/articlesPage/Tab.svelte
+++ src/components/articlesPage/Tab.svelte
@@ -3,6 +3,8 @@
 	import type { TabCtxType } from "./Tablist.svelte";
 	import { writable } from "svelte/store";
 	import { fade } from "svelte/transition";
+	import { onMount } from "svelte";
+	import { queryParam } from "sveltekit-search-params";
 
 	export let open: boolean = false;
 	export let title: string = "Tab title";
@@ -22,18 +24,47 @@
 
 		return { destroy };
 	}
+
+	let query = queryParam("tab", {
+		encode: (value: string) => value,
+		decode: (value) => value,
+	});
+
+	if ($query === title) {
+		open = true;
+	}
+
+	let sanitizedTitle = title.replace(/ /g, "-").replace(/[^a-zA-Z0-9-]/g, "");
+
+	function updateURL() {
+		$query = title
+		open = true;
+	}
+
+	onMount(() => {
+		if ($query === title) {
+			setTimeout(() => {
+				const el = document.getElementById(sanitizedTitle);
+				if (el) {
+					el.scrollIntoView({ behavior: "smooth", block: "center" });
+				}
+			}, 300);
+		}
+	})
 </script>
 
 <li role="presentation">
-	<button
-		type="button"
-		on:click={() => (open = true)}
-		role="tab"
-		class={"mr-4 px-1 text-xl hover:text-highlight dark:hover:text-highlight-dark duration-300 border-b-2 border-transparent" + (open ? 
-		" dark:text-highlight-dark text-highlight dark:border-highlight-dark border-highlight" : "")}
-		{...$$restProps}>
-		<slot name="title">{title}</slot>
-	</button>
+	<div class="flex" id="{sanitizedTitle}">
+		<button
+			type="button"
+			on:click={updateURL}
+			role="tab"
+			class={"mr-4 px-1 text-xl hover:text-highlight dark:hover:text-highlight-dark duration-300 border-b-2 border-transparent" + (open ?
+			" dark:text-highlight-dark text-highlight dark:border-highlight-dark border-highlight" : "")}
+			{...$$restProps}>
+			<slot name="title">{title}</slot>
+		</button>
+	</div>
 
 	{#if open}
 		<div class="hidden tab_content_placeholder">
