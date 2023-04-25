3b511d2bf3f5dd728ce3ddfa22199acc20e3d6f5
VitroidFPV <vitroid05@gmail.com>
2023-04-25 23:59:59 +0200

    Content: Added link to footer

--- src/components/navbar/Footer.svelte
+++ src/components/navbar/Footer.svelte
@@ -114,131 +114,6 @@
 		</svg>
 	</div>
 	<div class="w-full min-h-32 dark:bg-main-400 bg-contrast-50 md:pl-48 md:pb-8 pb-28 md:px-8">
-		<!-- <div class="text-contrast-100 flex md:flex-row flex-col md:items-start items-center">
-			<div
-				class="justify-self-center text-6xl font-caveat text-contrast-200 w-fit my-4 md:mr-12">
-				Vitroid FPV
-			</div>
-
-			<div class="md:flex hidden flex-col w-64">
-				<div class="text-2xl">Navigation:</div>
-				<ul class="text-lg">
-					<li>
-						<a href="/" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-							Home
-						</a>
-					</li>
-					<li>
-						<a href="/faq" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-							FAQ
-						</a>
-					</li>
-					<li>
-						<a href="/builds" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-							Builds
-						</a>
-					</li>
-					<li>
-						<a href="/equipment" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-							Equipment
-						</a>
-					</li>
-					<li>
-						<a href="/articles" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-							Articles
-						</a>
-					</li>
-				</ul>
-			</div>
-
-			<div class="flex flex-col md:w-64 my-4">
-				<div class="text-2xl md:text-left text-center">Socials:</div>
-				<div class="grid grid-rows-2 grid-cols-3 w-fit">
-					<div class="h-16 w-16 mr-2 mb-2">
-						<a
-							href="https://discord.gg/fpv"
-							class="w-fit h-fit flex flex-col opacity-90 hover:opacity-100 duration-100">
-							<div
-								class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/discord-logo.png')] bg-no-repeat bg-contain bg-center"
-							/>
-							<div class="text-xs opacity-50">Mr Steele</div>
-						</a>
-					</div>
-					
-					<div class="w-16 mr-2 mb-2">
-						<a
-							href="https://discord.gg/uavfutures"
-							class="w-fit h-fit flex flex-col opacity-90 hover:opacity-100 duration-100">
-							<div
-								class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/discord-logo.png')] bg-no-repeat bg-contain bg-center"
-							/>
-							<div class="text-xs opacity-50">UAVFutures</div>
-						</a>
-					</div>
-					<div class="w-16 mr-2 mb-2">
-						<a
-							href="https://www.youtube.com/@VitroidFPV"
-							class="w-fit h-fit flex flex-col items-center opacity-90 hover:opacity-100 duration-100 mr-4 text-[0px] leading-none">
-							YouTube
-							<div class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/youtube-play.png')] bg-no-repeat bg-contain bg-center"/>
-						</a>
-					</div>
-					<div class="w-16 mr-2 mb-2">
-						<a
-							href="https://discord.gg/drones"
-							class="w-fit h-fit flex flex-col opacity-90 hover:opacity-100 duration-100">
-							<div
-								class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/discord-logo.png')] bg-no-repeat bg-contain bg-center"
-							/>
-							<div class="text-xs opacity-50">Drone Community</div>
-						</a>
-					</div>
-					<div class="w-16 mr-2 mb-2">
-						<a
-							href="https://discord.gg/n4E6ak4u3c"
-							class="w-fit h-fit flex flex-col opacity-90 hover:opacity-100 duration-100">
-							<div
-								class="h-12 w-12 bg-[url('https://img.icons8.com/material-rounded/64/eeeeee/discord-logo.png')] bg-no-repeat bg-contain bg-center"
-							/>
-							<div class="text-xs opacity-50">Betaflight</div>
-						</a>
-					</div>
-					<div class="w-16 mr-2 mb-2">
-						<a
-							href="https://github.com/VitroidFPV/VitroidFPV"
-							class="w-fit h-fit flex flex-col items-center opacity-90 hover:opacity-100 duration-100 mr-4 mb-3 text-[0px] leading-none">
-							GitHub
-							<div class="h-12 w-12 bg-[url('https://img.icons8.com/ios-filled/64/eeeeee/github--v1.png')] bg-no-repeat bg-contain bg-center"/>
-						</a>
-					</div>
-				</div>
-			</div>
-
-			<div class="flex md:flex-col">
-
-				<div class="flex flex-col mt-2">
-					<div class="text-2xl text-center md:text-left">Other resources:</div>
-					<ul class="text-base text-center md:text-left">
-						<li>
-							<a href="https://www.youtube.com/@JoshuaBardwell" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-								Joshua Bardwell
-							</a> - Reviews and tutorials
-						</li>
-						<li>
-							<a href="https://oscarliang.com/" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-								Oscar Liang
-							</a> - Reviews and tutorials
-						</li>
-						<li>
-							<a href="https://fpvmarket.eu/" class="underline font-thin opacity-90 hover:opacity-100 duration-100">
-								FPV Market
-							</a> - EU FPV store search
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div> -->
-
 		<footer>
 			<div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
 				<div class="md:flex md:justify-between">
@@ -354,17 +229,16 @@
 									Contributors
 								</LinkDetail>
 							</li>
-							<!-- <li>
+							<li>
 								<LinkDetail
-									href="/"
-									description="Support me through donations"
-									external
+									href="/support"
+									description="Support development of the site"
 									color="red"
 									color1="red"
 								>
 									Support me
 								</LinkDetail>
-							</li> -->
+							</li>
 						</ul>
 					</div>
 				</div>
