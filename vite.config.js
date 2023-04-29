e2c5932a9ea0866916edf0f81ca83d2cb0a61951
VitroidFPV <vitroid05@gmail.com>
2023-04-29 23:43:55 +0200

    Fix: Fix attempt #1

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
-// const viteServerConfig = {
-// 	name: "vite-server",
-// 	configureServer(server) {
-// 		server.middlewares.use((req, res, next) => {
-// 			// res.setHeader("Access-Control-Allow-Origin", "*");
-// 			// Cross-Origin-Embedder-Policy: require-corp
-// 			// Cross-Origin-Opener-Policy: same-origin
-
-// 			res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-// 			res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-
-// 			next();
-// 		});
-// 	},
-// };
-
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit(), 
@@ -36,11 +20,11 @@ const config = {
 			allow: ["./modules/"]
 		},
 		port: 3000,
-		// headers: {
-		// 	"Cross-Origin-Embedder-Policy": "require-corp",
-		// 	"Cross-Origin-Opener-Policy": "same-origin",
-		// 	"Cross-Origin-Resource-Policy": "cross-origin",
-		// }
+		headers: {
+			"Cross-Origin-Embedder-Policy": "require-corp",
+			"Cross-Origin-Opener-Policy": "same-origin",
+			"Cross-Origin-Resource-Policy": "cross-origin",
+		}
 	},
 	resolve: {
 		alias: {
