7dd90a0d54c81e1116c28b41f7173b01501ed715
VitroidFPV <vitroid05@gmail.com>
2023-11-11 00:45:14 +0100

    Style: More custom Button styles

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let color: "red" | "orange" | "yellow" | "green" | "cyan" | "blue" | "violet" = "green";
+	export let color: "red" | "orange" | "yellow" | "green" | "cyan" | "blue" | "violet" | "highlight" = "green";
 	export let isLink: boolean;	
 	export let href: string = "";
 
@@ -12,7 +12,7 @@
 	let colors: Colors
 	colors = {
 		bg: "bg-green",
-		bgTransparent: "bg-green/20 hover:bg-green/40",
+		bgTransparent: "bg-green/10 hover:bg-green/20",
 		text: "text-green",
 	}
 
@@ -20,57 +20,80 @@
 		case "red":
 			colors = {
 				bg: "bg-red",
-				bgTransparent: "bg-red/20 hover:bg-red/40",
+				bgTransparent: "bg-red/10 hover:bg-red/20",
 				text: "text-red",
 			};
 			break;
 		case "orange":
 			colors = {
 				bg: "bg-orange",
-				bgTransparent: "bg-orange/20 hover:bg-orange/40",
+				bgTransparent: "bg-orange/10 hover:bg-orange/20",
 				text: "text-orange",
 			};
 			break;
 		case "yellow":
 			colors = {
 				bg: "bg-yellow",
-				bgTransparent: "bg-yellow/20 hover:bg-yellow/40",
+				bgTransparent: "bg-yellow/10 hover:bg-yellow/20",
 				text: "text-yellow",
 			};
 			break;
 		case "green":
 			colors = {
 				bg: "bg-green",
-				bgTransparent: "bg-green/20 hover:bg-green/40",
+				bgTransparent: "bg-green/10 hover:bg-green/20",
 				text: "text-green",
 			};
 			break;
 		case "cyan":
 			colors = {
 				bg: "bg-cyan",
-				bgTransparent: "bg-cyan/20 hover:bg-cyan/40",
+				bgTransparent: "bg-cyan/10 hover:bg-cyan/20",
 				text: "text-cyan",
 			};
 			break;
 		case "blue":
 			colors = {
 				bg: "bg-blue",
-				bgTransparent: "bg-blue/20 hover:bg-blue/40",
+				bgTransparent: "bg-blue/10 hover:bg-blue/20",
 				text: "text-blue",
 			};
 			break;
 		case "violet":
 			colors = {
 				bg: "bg-violet",
-				bgTransparent: "bg-violet/20 hover:bg-violet/40",
+				bgTransparent: "bg-violet/10 hover:bg-violet/20",
 				text: "text-violet",
 			};
 			break;
+		case "highlight":
+			colors = {
+				bg: "bg-highlight",
+				bgTransparent: "bg-highlight/10 hover:bg-highlight/20 dark:bg-highlight-dark/10 dark:hover:bg-highlight-dark/20",
+				text: "text-highlight dark:text-highlight-dark",
+			};
+			break;
 	}
+
+	export let size: "sm" | "md" | "lg" = "md";
+
+	switch (size) {
+		case "sm":
+			colors.bgTransparent += " p-2 outline-[3]";
+			break;
+		case "md":
+			colors.bgTransparent += " md:p-4 p-3 outline-4";
+			break;
+		case "lg":
+			colors.bgTransparent += " md:p-6 p-4 outline-4";
+			break;
+	}
+
+	export let type: "button" | "submit" | "reset" = "button";
 </script>
 
 {#if isLink}
-	<a {href} class="{colors.bgTransparent} {colors.text} backdrop-blur-md md:p-4 p-3 rounded-full outline outline-4 outline-current duration-300 shadow-xl text-base"><span class="dark:text-inherit text-black/75"><slot/></span></a>
+	<a {href} class="{colors.bgTransparent} {colors.text} backdrop-blur-md rounded-full outline outline-current duration-300 shadow-xl text-base"><span class="dark:text-inherit text-black/75"><slot/></span></a>
 {:else}
-	<button on:click class="{colors.bgTransparent} {colors.text} backdrop-blur-md md:p-4 p-3 rounded-full outline outline-4 outline-current duration-300 shadow-xl text-base"><span class="dark:text-inherit text-black/75"><slot/></span></button>
+	<button {type} on:click class="{colors.bgTransparent} {colors.text} backdrop-blur-md rounded-full outline outline-current duration-300 shadow-xl text-base"><span class="dark:text-inherit text-black/75"><slot/></span></button>
 {/if}
