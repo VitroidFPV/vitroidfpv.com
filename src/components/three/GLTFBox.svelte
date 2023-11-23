873987f5611a484ca4a7ba178d54d2daa23244f7
VitroidFPV <vitroid05@gmail.com>
2023-11-24 00:08:16 +0100

    Fix: Sometimes the dumbest solution is the best

--- src/components/three/GLTFBox.svelte
+++ src/components/three/GLTFBox.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import { Canvas } from "@threlte/core";
 	import GLTFWrapper from "$components/three/GLTFWrapper.svelte";
+	import IntersectionObserver from "svelte-intersection-observer";
+	import { fade } from "svelte/transition";
+
+	let element: HTMLElement;
+	let intersecting: boolean;
 
 	export let url: string = "";
 	export let position: [number, number, number] = [0, 0, 0];
@@ -8,15 +13,23 @@
 	export let scale: number = 1;
 </script>
 
-<div>
-	<div 
-		class="aspect-square rounded-2xl bg-neutral-400/5 mb-3 overflow-clip outline outline-2 outline-neutral-400/20
-		cursor-grab">
-		<Canvas useLegacyLights={false}>
-			<slot>
-				<!-- <GLTFWrapper {url} {position} {rotation} {scale} /> -->
-			</slot>
-		</Canvas>
+
+<IntersectionObserver {element} bind:intersecting>
+	<div bind:this={element}>
+		<div 
+			class="aspect-square rounded-2xl bg-neutral-400/5 mb-3 overflow-clip outline outline-2 outline-neutral-400/20
+			cursor-grab"
+		>
+			{#if intersecting}
+				<div transition:fade={{duration: 300}} class="w-full h-full">
+					<Canvas useLegacyLights={false}>
+						<slot>
+							<!-- <GLTFWrapper {url} {position} {rotation} {scale} /> -->
+						</slot>
+					</Canvas>
+				</div>
+			{/if}
+		</div>
+		<slot name="content" />
 	</div>
-	<slot name="content" />
-</div>
+</IntersectionObserver>
