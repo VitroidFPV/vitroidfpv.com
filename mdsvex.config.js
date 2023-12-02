5e62f395d30bc693af65d1e6868f75a6f16252b1
VitroidFPV <vitroid05@gmail.com>
2023-12-02 18:59:59 +0100

    Feature: Added math

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,18 +1,21 @@
 // import admonitions from "remark-admonitions";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex-svelte";
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
 
-const config = {
+const config = defineConfig({
 	extensions: [".svelte.md", ".md", ".svx"],
 
 	smartypants: {
 		dashes: "oldschool"
 	},
-	remarkPlugins: [remarkGfm],
-	rehypePlugins: [rehypeSlug],
+	remarkPlugins: [remarkGfm, remarkMath],
+	rehypePlugins: [rehypeSlug, rehypeKatex],
 	layout: {
 		_: "/src/components/articlesPage/ArticleLayout.svelte"
 	}
-};
+});
 
 export default config;
