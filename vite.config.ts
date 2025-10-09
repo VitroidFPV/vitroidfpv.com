2ee9e9306b4368aa809618cbd36861f2b30ef8e7
Vitroid <vitroid05@gmail.com>
2025-10-09 22:23:20 +0200

    Package: Install and implement vite-plugin-yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
+import ViteYaml from '@modyfi/vite-plugin-yaml';
 
 export function customHmr() {
 	return {
@@ -21,7 +22,7 @@ export function customHmr() {
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [tailwindcss(), sveltekit(), customHmr()],
+	plugins: [tailwindcss(), sveltekit(), customHmr(), ViteYaml()],
 	server: {
 		fs: {
 			allow: ["./modules/", "./admin/", '../..']
