e06c752f5ca49a651b9d2baf18e5fea1eaaadda9
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2026-07-07 02:00:33 +0200

    UI: Fancy link hover effect

--- src/components/ListShowcase.svelte
+++ src/components/ListShowcase.svelte
@@ -57,7 +57,7 @@
 {/snippet}
 
 <div
-	class="group relative flex h-full w-full flex-col overflow-hidden rounded-2xl border-2 border-surface-100-900 transition-all duration-300 hover:shadow-2xl {colors.cardHover} lg:min-h-96 lg:flex-row lg:rounded-[3rem] 2xl:min-h-128"
+	class="group relative flex h-full w-full flex-col overflow-hidden rounded-2xl border-2 border-surface-100-900 transition-all duration-300 hover:shadow-2xl {colors.cardHover} bg-surface-50-950 lg:min-h-96 lg:flex-row lg:rounded-[3rem] 2xl:min-h-128"
 	style:--build-accent={buildAccentVar[color]}
 >
 	<div class="relative z-10 flex-1 lg:max-w-2/3 xl:min-w-3xl">
@@ -65,14 +65,15 @@
 			<img
 				src={gggyrate}
 				alt=""
-				class="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 scale-300 opacity-10 transition-all duration-300 lg:scale-200 lg:opacity-5 lg:group-hover:scale-205"
+				class="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 scale-300 opacity-10 transition-all duration-300 group-hover:opacity-10 lg:scale-200 lg:opacity-5 lg:group-hover:scale-205
+				"
 			/>
 			<div
 				class="text-container relative flex h-fit w-full flex-col gap-2 p-2 md:p-4 lg:gap-4 lg:p-8"
 			>
 				<div class="flex w-full items-center justify-between">
 					<h3
-						class="font-josefin-sans font-bold {colors.text} -mb-2 pt-2 text-4xl lg:mb-0 lg:pt-0 xl:text-5xl 2xl:text-8xl"
+						class="font-josefin-sans font-bold {colors.text} fancy-link-xl -mb-2 pt-2 text-4xl lg:mb-0 lg:pt-0 xl:text-5xl 2xl:text-8xl"
 					>
 						{title}
 					</h3>
@@ -159,17 +160,33 @@
 		}
 	}
 
+	@property --accent-gradient-opacity {
+		syntax: "<number>";
+		inherits: false;
+		initial-value: 1;
+	}
+
 	.img-container {
 		--s: 100px; /* control the size*/
 		--c1: rgba(255, 255, 255, 0.01);
 		--c2: transparent;
+		--accent-gradient-opacity: 1;
 
 		--_g: #0000 90deg, var(--c1) 0;
 		background:
 			radial-gradient(
 				at 100% 100%,
-				color-mix(in oklch, var(--build-accent) 12%, transparent),
-				color-mix(in oklch, var(--build-accent) 5%, transparent) 50%,
+				color-mix(
+					in oklch,
+					var(--build-accent) calc(12% * var(--accent-gradient-opacity)),
+					transparent
+				),
+				color-mix(
+						in oklch,
+						var(--build-accent) calc(5% * var(--accent-gradient-opacity)),
+						transparent
+					)
+					50%,
 				transparent 70%
 			),
 			conic-gradient(from 90deg at 1px 1px, var(--_g)),
@@ -178,5 +195,10 @@
 			auto,
 			var(--s) var(--s),
 			calc(var(--s) / 5) calc(var(--s) / 5);
+		transition: --accent-gradient-opacity 300ms ease;
+	}
+
+	.group:hover .img-container {
+		--accent-gradient-opacity: 1.2;
 	}
 </style>
