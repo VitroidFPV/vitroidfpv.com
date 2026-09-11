95e0e91feec2e9c843e4b66e964a6d93cbfb333f
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2026-09-11 23:40:33 +0200

    Content: Scaffold video gear guide

---
+++ src/routes/equipment/video/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import BuildGuidePage from "$components/build-guides/BuildGuidePage.svelte"
+	import { getEquipmentGuide } from "$lib/guides/content"
+</script>
+
+<BuildGuidePage guide={getEquipmentGuide("video")} />
