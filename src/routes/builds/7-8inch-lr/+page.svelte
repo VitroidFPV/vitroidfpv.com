a972eeeae200726cf7093daea46372bc1e8d092c
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2026-09-11 23:40:07 +0200

    Refactor: Accept guide object in guide pages

--- src/routes/builds/7-8inch-lr/+page.svelte
+++ src/routes/builds/7-8inch-lr/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import BuildGuidePage from "$components/build-guides/BuildGuidePage.svelte"
+	import { getBuildGuide } from "$lib/guides/content"
 </script>
 
-<BuildGuidePage guideSlug="7-8inch-lr" />
+<BuildGuidePage
+	guide={getBuildGuide("7-8inch-lr")}
+	editable
+/>
